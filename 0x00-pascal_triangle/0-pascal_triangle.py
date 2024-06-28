0d2b0e12b36f848ab57512b2dc3dda6eb1f7e7eb
deenakamal <deenah.kamal@gmail.com>
2024-06-28 02:01:30 +0000

    fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ pascal_triangle function"""
 
+
 def pascal_triangle(n):
     """
     Returns list of integer lists representing the Pascal's triangle
@@ -13,12 +14,12 @@ def pascal_triangle(n):
 
     for row_index in range(1, n):
         current_row = [1]
-        
+
         for col_index in range(1, row_index):
             left_value = result[row_index - 1][col_index - 1]
             right_value = result[row_index - 1][col_index]
             current_row.append(left_value + right_value)
-        
+
         current_row.append(1)
         result.append(current_row)
 
